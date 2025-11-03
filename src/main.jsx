c851f1c50872c86fbf319faaa8bddab6c6534dba
farazhayat2 <farazhayat448@gmail.com>
2025-11-03 19:07:58 +0500

    fixed again

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import StoreContextProvider from './Context/StoreContext.jsx';
+import { StoreContextProvider } from './Context/StoreContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
